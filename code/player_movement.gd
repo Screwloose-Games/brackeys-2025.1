530c79d52656045fbed735b72b0a4eaf316edaac
AJ Powell <ajpowell021@gmail.com>
2025-02-17 09:35:01 -0500

    Player Jumping
    - Player can now hit spacebar to jump.
    - Jump power in configurable in editor.

--- code/player_movement.gd
+++ code/player_movement.gd
@@ -2,8 +2,9 @@ extends Node3D
 
 @export var root: CharacterBody3D
 @export var speed: float
-@export var test: int
+@export var jump_power: float
 @export var mouse_sensitivity: float
+@export var fall_speed: float
 @export var camera_mount: SpringArm3D
 var target_velocity: Vector3 = Vector3.ZERO
 
@@ -12,7 +13,7 @@ var direction: Vector3 = Vector3.ZERO
 func _ready():
     Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
-func _physics_process(_delta: float):
+func _physics_process(delta: float):
     direction = Vector3.ZERO
     
     if InputManager.input_mode == InputManager.InputMode.PLAYING:
@@ -24,15 +25,21 @@ func _physics_process(_delta: float):
             direction.x += 1
         if Input.is_action_pressed("Move_Left"):
             direction.x -= 1
+        
+        if root.is_on_floor() and Input.is_action_just_pressed("Jump"):
+            target_velocity.y = jump_power
     
-    direction = direction.rotated(Vector3.UP, camera_mount.rotation.y)		
+    direction = direction.rotated(Vector3.UP, camera_mount.rotation.y)
 
     if direction != Vector3.ZERO:
         direction = direction.normalized()
         
     target_velocity.x = direction.x * speed
     target_velocity.z = direction.z * speed
-    
+
+    if not root.is_on_floor():
+        target_velocity.y = target_velocity.y - (fall_speed * delta)
+
     root.velocity = target_velocity
     root.move_and_slide()
 

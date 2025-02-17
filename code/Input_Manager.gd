dc9cf24ff9cbd6fc6b898ba3188b0249fcb06322
AJ Powell <ajpowell021@gmail.com>
2025-02-17 09:04:30 -0500

    Basic Camera Movement
    - Can now use mouse to look around
    - Camera now follows player
    - Hide the mouse cursor and confined it to the game screen
    - Pressing escape exits the game

--- code/Input_Manager.gd
+++ code/Input_Manager.gd
@@ -3,6 +3,10 @@ extends Node3D
 enum InputMode { PLAYING }
     
 var input_mode: InputMode = InputMode.PLAYING;
-    
+
+func _input(event: InputEvent):
+    if event.is_action_pressed("Close Game"):
+        get_tree().quit()
+
 func set_input_mode(new_mode: InputMode):
     input_mode = new_mode
--- code/player_movement.gd
+++ code/player_movement.gd
@@ -2,28 +2,42 @@ extends Node3D
 
 @export var root: CharacterBody3D
 @export var speed: float
+@export var test: int
+@export var mouse_sensitivity: float
+@export var camera_mount: SpringArm3D
 var target_velocity: Vector3 = Vector3.ZERO
 
 var direction: Vector3 = Vector3.ZERO
 
+func _ready():
+    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
 func _physics_process(_delta: float):
-	direction = Vector3.ZERO
-	
-	if InputManager.input_mode == InputManager.InputMode.PLAYING:
-		if Input.is_action_pressed("Move_Forward"):
-			direction.z -= 1
-		if Input.is_action_pressed("Move_Backward"):
-			direction.z += 1
-		if Input.is_action_pressed("Move_Right"):
-			direction.x += 1
-		if Input.is_action_pressed("Move_Left"):
-			direction.x -= 1
-	
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		
-	target_velocity.x = direction.x * speed
-	target_velocity.z = direction.z * speed
-	
-	root.velocity = target_velocity
-	root.move_and_slide()
+    direction = Vector3.ZERO
+    
+    if InputManager.input_mode == InputManager.InputMode.PLAYING:
+        if Input.is_action_pressed("Move_Forward"):
+            direction.z -= 1
+        if Input.is_action_pressed("Move_Backward"):
+            direction.z += 1
+        if Input.is_action_pressed("Move_Right"):
+            direction.x += 1
+        if Input.is_action_pressed("Move_Left"):
+            direction.x -= 1
+    
+    direction = direction.rotated(Vector3.UP, camera_mount.rotation.y)		
+
+    if direction != Vector3.ZERO:
+        direction = direction.normalized()
+        
+    target_velocity.x = direction.x * speed
+    target_velocity.z = direction.z * speed
+    
+    root.velocity = target_velocity
+    root.move_and_slide()
+
+func _unhandled_input(event: InputEvent):
+    if event is InputEventMouseMotion:
+        camera_mount.rotation.x -= event.relative.y * mouse_sensitivity
+        camera_mount.rotation_degrees.x = clamp(camera_mount.rotation_degrees.x, -90.0, 30.0)
+        camera_mount.rotation.y -= event.relative.x * mouse_sensitivity

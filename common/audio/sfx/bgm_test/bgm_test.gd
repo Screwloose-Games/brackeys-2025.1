2a16dd470e66f307693238ba18f248c824f77b35
MechaWeeg <mechaweeg@gmail.com>
2025-02-19 01:35:27 +0100

    Audio manager work
    Can now adjust volume, has different audio busses and audio players, can have a bank of sounds for one action.

---
+++ common/audio/sfx/bgm_test/bgm_test.gd
@@ -0,0 +1,42 @@
+extends Control
+
+signal walk
+signal run
+signal jump
+signal slide
+signal music_change(track)
+
+@onready var music_volume: HSlider = $VBoxContainer/MusicVol
+@onready var sfx_volume: HSlider = $VBoxContainer/SFXVol
+
+func _ready() -> void:
+	AudioManager.connect_audio_signals(self)
+	music_volume.value = AudioServer.get_bus_volume_linear(2)
+	sfx_volume.value = AudioServer.get_bus_volume_linear(1)
+	
+
+func _on_walk_pressed() -> void:
+	walk.emit()
+
+func _on_run_pressed() -> void:
+	run.emit()
+
+func _on_jump_pressed() -> void:
+	jump.emit()
+
+func _on_slide_pressed() -> void:
+	slide.emit()
+
+func _on_sneak_music_pressed() -> void:
+	music_change.emit("sneak")
+
+func _on_action_music_pressed() -> void:
+	music_change.emit("action")
+
+
+func _on_music_vol_value_changed(value: float) -> void:
+	AudioManager.set_volume(AudioManager.AudioBusType.MUSIC, value)
+
+
+func _on_sfx_vol_value_changed(value: float) -> void:
+	AudioManager.set_volume(AudioManager.AudioBusType.SFX, value)
--- globals/audio_manager/audio_manager.gd
+++ globals/audio_manager/audio_manager.gd
@@ -1,10 +1,59 @@
 extends Node
 
-@onready var sfx_player: AudioStreamPlayer = $SfxPlayer
-
-var sfx_dictionary = {
-	"jump": "PATH TO SOUND",
+enum SoundCategory {
+	PLAYER,
+	MUSIC,
+	ENVIRONMENT,
+	UI
 }
+
+enum AudioBusType {
+	MASTER, 
+	SFX, 
+	MUSIC, 
+	ENVIRONMENT
+}
+
+@onready var player_sounds: AudioStreamPlayer = $PlayerSounds
+@onready var music: AudioStreamPlayer = $Music
+@onready var environment_sounds: AudioStreamPlayer = $EnvironmentSounds
+@onready var ui_sounds: AudioStreamPlayer = $UISounds
+
+
+var audio_players: Dictionary = {}
+
+var sfx_dictionary: Dictionary = {
+	"jump": {
+		"stream": [
+			preload("res://common/audio/sfx/player/jump/jump1.wav"), 
+			preload("res://common/audio/sfx/player/jump/jump2.wav"), 
+			preload("res://common/audio/sfx/player/jump/jump3.wav")
+		],
+		"category": SoundCategory.PLAYER
+	},
+	"slide": {
+		"stream": preload("res://common/audio/sfx/player/slide/Swoosh4.wav"),
+		"category": SoundCategory.PLAYER
+	},
+	"walk": {
+		"stream": preload("res://common/audio/sfx/player/walk/Walking Loop - Flooring.wav"),
+		"category": SoundCategory.PLAYER
+	}
+}
+
+var music_dictionary: Dictionary = {
+	"light": "PATH TO TRACK",
+	"sneak": preload("res://common/audio/music/TEMPSneakTime.mp3"),
+	"action": preload("res://common/audio/music/TEMP-Rush.mp3"),
+}
+
+func _ready() -> void:
+	audio_players = {
+		SoundCategory.PLAYER: player_sounds,
+		SoundCategory.MUSIC: music,
+		SoundCategory.ENVIRONMENT: environment_sounds,
+		SoundCategory.UI: ui_sounds
+	}
 
 func connect_audio_signals(node: Node):
 	print(node," is being checked for signals")
@@ -12,17 +61,65 @@ func connect_audio_signals(node: Node):
 		if node.has_signal(sfx_request):
 			if !node.is_connected(sfx_request, _play_sound):
 				node.connect(sfx_request, _play_sound.bind(sfx_request))
+		if node.has_signal("music_change"):
+			if !node.is_connected("music_change", _play_music):
+				node.connect("music_change", _play_music)
+				print(node, " has grabbed the aux")
+				
 	for child in node.get_children():
 		if child is Node: #TODO decide a better class to check for
 			connect_audio_signals(child)
-				
-func _on_node_exited():
-	pass
 
 func _play_sound(sfx_request):
-	sfx_player.stream = load(sfx_dictionary[sfx_request]) #TODO make these preloads
-	if !sfx_player.stream:
-		print(sfx_request," does not exist")
+	var sfx_data = sfx_dictionary[sfx_request]
+	if !sfx_data:
+		print(sfx_request, " does not exist")
+		return
+	
+	var stream_path = sfx_data["stream"]
+	if stream_path is Array:
+		stream_path = stream_path[randi() % stream_path.size()]
+	
+	var category = sfx_data["category"]
+	var audio_player = audio_players.get(category)
+	if audio_player == null:
+		print("Invalid category: ", category)
+		return
+	
+	audio_player.stream = stream_path
+	
+	if !audio_player.stream:
+		print(sfx_request, " does not exist")
 		return
 	else:
-		sfx_player.play()
+		audio_player.play()
+
+func _play_music(music_request):
+	music.stream = music_dictionary[music_request]
+	music.play()
+
+func get_volume(bus: AudioBusType):
+	match(bus):
+		AudioBusType.MASTER:
+			return AudioServer.get_bus_volume_db(0)
+		AudioBusType.SFX:
+			return AudioServer.get_bus_volume_db(1)
+		AudioBusType.MUSIC:
+			return AudioServer.get_bus_volume_db(2)
+		AudioBusType.ENVIRONMENT:
+			return AudioServer.get_bus_volume_db(3)
+		_:
+			return AudioServer.get_bus_volume_db(0)
+
+func set_volume(bus, value: float):
+	match(bus):
+		AudioBusType.MASTER:
+			AudioServer.set_bus_volume_db(0, linear_to_db(value))
+		AudioBusType.SFX:
+			AudioServer.set_bus_volume_db(1, linear_to_db(value))
+		AudioBusType.MUSIC:
+			AudioServer.set_bus_volume_db(2, linear_to_db(value))
+		AudioBusType.ENVIRONMENT:
+			AudioServer.set_bus_volume_db(3, linear_to_db(value))
+		_:
+			AudioServer.set_bus_volume_db(0, linear_to_db(value))

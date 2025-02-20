c4aed2148d99c29279a59ad7d69aff3a4d11e523
AJ Powell <ajpowell021@gmail.com>
2025-02-20 12:36:50 -0500

    Add Loop NPC Action
    New enum for NPC actions that cause the NPC to loop back to a previous action. Can use this to make it so NPCs carry out specific actions and then loop between a couple.

--- code/npc_action.gd
+++ code/npc_action.gd
@@ -6,13 +6,17 @@ enum Type {
     MOVE,
     RETURN_TO_STARTING_POSITION,
     PICKS_UP_RINGS,
+    LOOP,
 }
 
 @export var action_type: Type = Type.WAIT
 @export var wait_time: float
 @export var destination: Vector3
+@export var loop_to_step: int
 
-func _init(_action_type:= Type.WAIT, _wait_time:= 0, _destination:=Vector3.ZERO):
+func _init(_action_type:= Type.WAIT, _wait_time:= 0, _destination:=Vector3.ZERO, _loop_to_step:=0):
     action_type = _action_type
     wait_time = _wait_time
     destination = _destination
+    loop_to_step = _loop_to_step
+    

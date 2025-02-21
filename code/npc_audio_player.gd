3d8968a2d26fd0a000c77f66ab3088722c6c0200
MechaWeeg <mechaweeg@gmail.com>
2025-02-21 21:46:11 +0100

    Music fades fixed, new npc sounds
    example: npc_audio.play_sound(NpcAudioPlayer.NpcSounds.INTERACT)

--- code/npc_audio_player.gd
+++ code/npc_audio_player.gd
@@ -1,38 +1,118 @@
 extends AudioStreamPlayer3D
 class_name NpcAudioPlayer
 
+enum NpcSounds {
+	INTERACT,
+	WALK,
+	UNDERSTANDING,
+	CRYING,
+	PAIN,
+	DISAGREE,
+	SLY_LAUGH,
+	MMM,
+	DISCOVERY,
+	YAWN,
+	HEHEH,
+	QUESTION,
+	INTEREST
+}
+
 @export var npc_root: CharacterBody3D
 
 var masc_sound_dictionary: Dictionary = {
-	"interact": {
+	NpcSounds.INTERACT: {
 		"stream": "res://scenes/npc/guest_masc/sounds/masc_interact_randomizer.tres",
 	},
-	"walk": {
+	NpcSounds.WALK: {
 		"stream": "res://common/audio/sfx/npc/npc_footsteps.tres",
+	},
+	NpcSounds.UNDERSTANDING: {
+		"stream": "res://scenes/npc/best_man/sounds/adult_masc_uh_huh.wav",
+	},
+	NpcSounds.CRYING: {
+		"stream": "res://scenes/npc/groom/sounds/adult_masc_crying.wav",
+	},
+	NpcSounds.PAIN: {
+		"stream": "res://scenes/npc/groom/sounds/adult_masc_pain.wav",
+	},
+	NpcSounds.DISAGREE: {
+		"stream": "res://scenes/npc/technician/sounds/adult_masc_grumpy.wav",
+	},
+	NpcSounds.SLY_LAUGH: {
+		"stream": "res://scenes/npc/guest_masc/sounds/adult_masc_sly_laugh.wav",
+	},
+	NpcSounds.MMM: {
+		"stream": "res://scenes/npc/technician/sounds/adult_masc_indeed.wav",
+	},
+	NpcSounds.DISCOVERY: {
+		"stream": "res://scenes/npc/uncle_bob/sounds/adult_masc_discovery_ooo.wav",
+	},
+	NpcSounds.YAWN: {
+		"stream": "res://scenes/npc/guest_masc/sounds/adult_masc_yawn.wav",
+	},
+	NpcSounds.HEHEH: {
+		"stream": "res://scenes/npc/uncle_bob/sounds/adult_masc_heheh.wav",
+	},
+	NpcSounds.INTEREST: {
+		"stream": "res://scenes/npc/uncle_bob/sounds/adult_masc_interested_oh.wav",
+	},
+	NpcSounds.QUESTION: {
+		"stream": "res://scenes/npc/uncle_bob/sounds/adult_masc_questioning_huh.wav",
 	},
 }
 
 var femme_sound_dictionary: Dictionary = {
-	"interact": {
+	NpcSounds.INTERACT: {
 		"stream": "res://scenes/npc/guest_femme/sounds/femme_interact_randomizer.tres",
 	},
-	"walk": {
+	NpcSounds.WALK: {
 		"stream": "res://common/audio/sfx/npc/npc_footsteps.tres",
+	},
+	NpcSounds.UNDERSTANDING: {
+		"stream": "res://scenes/npc/guest_femme/sounds/adult_femme_understanding.wav",
+	},
+	NpcSounds.CRYING: {
+		"stream": "res://scenes/npc/bride/sounds/adult_femme_crying.wav",
+	},
+	NpcSounds.PAIN: {
+		"stream": "res://scenes/npc/guest_femme/sounds/adult_femme_injured.wav",
+	},
+	NpcSounds.DISAGREE: {
+		"stream": "res://scenes/npc/bride/sounds/adult_femme_disagree.wav",
+	},
+	NpcSounds.SLY_LAUGH: {
+		"stream": "res://scenes/npc/guest_femme/sounds/adult_femme_sly_laugh.wav",
+	},
+	NpcSounds.MMM: {
+		"stream": "res://scenes/npc/bride/sounds/adult_femme_mmm.wav",
+	},
+	NpcSounds.DISCOVERY: {
+		"stream": "res://scenes/npc/guest_femme/sounds/adult_femme_discovery.wav",
+	},
+	NpcSounds.YAWN: {
+		"stream": "res://scenes/npc/guest_femme/sounds/adult_femme_yawn.wav",
+	},
+	NpcSounds.HEHEH: {
+		"stream": "res://scenes/npc/guest_femme/sounds/adult_femme_chuckle.wav",
+	},
+	NpcSounds.QUESTION: {
+		"stream": "res://scenes/npc/woman_in_white/sounds/adult_femme_question.wav",
+	},
+	NpcSounds.INTEREST: {
+		"stream": "res://scenes/npc/guest_femme/sounds/adult_femme_interested.wav",
 	},
 }
 
 var used_dictionary: Dictionary = {}
 
-func _ready() -> void:
+func _get_dictionary() -> Dictionary:
 	if npc_root.is_masc:
-		used_dictionary = masc_sound_dictionary
-	else:
-		used_dictionary = femme_sound_dictionary
-	for key in used_dictionary.keys():
-		var stream_path = used_dictionary[key].get("stream", "")
-		if stream_path != "":
-			used_dictionary[key]["stream"] = load(stream_path)
+		return masc_sound_dictionary 
+	else: 
+		return femme_sound_dictionary
 
-func interact():
-	stream = used_dictionary["interact"].get("stream")
-	play()
+func play_sound(sound: NpcSounds) -> void:
+	var used_dictionary = _get_dictionary()
+	if sound in used_dictionary:
+		stream = load(used_dictionary[sound].get("stream"))
+		play()
